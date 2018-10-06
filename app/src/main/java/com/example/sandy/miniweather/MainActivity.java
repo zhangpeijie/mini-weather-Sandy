31a8a407a6c6a77554be6892415ee1c602346bf1
unknown <zhangpeijie@pku.edu.cn name=Sandy>
2018-10-06 21:53:19 +0800

    init project

---
+++ app/src/main/java/com/example/sandy/miniweather/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.sandy.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
